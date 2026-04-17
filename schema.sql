84afdab2e99e443adae34e4241d579a1694a1b43
rajaryanbanka1 <79257922+rajaryanbanka1@users.noreply.github.com>
2026-04-17 19:41:29 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(50),
+    signup_date DATE
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount DECIMAL(10,2),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    order_id INT,
+    payment_method VARCHAR(50),
+    status VARCHAR(20),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+CREATE TABLE transactions (
+    txn_id INT PRIMARY KEY,
+    customer_id INT,
+    txn_date DATE,
+    txn_amount DECIMAL(10,2),
+    txn_type VARCHAR(20)
+);
